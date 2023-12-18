f7dd41b1ac814359f086e981f55934966a328fef
ian <me@iany.me>
2023-12-18 13:12:20 +0800

    secp256r1 pubkey formats

---
+++ secp256r1.py
@@ -0,0 +1,15 @@
+from rich import print, box
+from rich.console import Console
+from rich.panel import Panel
+from Crypto.PublicKey import ECC
+
+console = Console()
+
+key = ECC.generate(curve="secp256r1")
+pubkey = key.public_key()
+
+print(Panel("pubkey.pem", box=box.SQUARE))
+print(pubkey.export_key(format="PEM"))
+
+print(Panel("pubkey.sec1", box=box.SQUARE))
+console.print(pubkey.export_key(format="SEC1").hex(), width=66)
